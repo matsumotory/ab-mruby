b385d629c4933cc73756ae9af1b5840a82c06546
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2013-06-06 18:54:03 +0900

    Add sample test suite

--- ab-mruby.test.rb
+++ ab-mruby.test.rb
@@ -13,5 +13,39 @@ Licensed to MATSUMOTO Ryosuke, https://github.com/matsumoto-r/ab-mruby
 ======================================================================
 EOS
 
+module Kernel
+  def test_suite &blk
+    @@r = get_config
+    @@t = blk
+  end
+  def should_be val
+    puts "[TEST CASE] #{self} (#{@@r[self]}) should be #{val}: #{@@r[self] == val}"
+  end
+  def should_be_over val
+    puts "[TEST CASE] #{self} (#{@@r[self]}) should be over #{val}: #{@@r[self] > val}"
+  end
+  def run
+    @@t.call
+  end
+end
+
 # get config value from C
-p get_config
+test_suite do
+  "TargetServerHost".should_be               "192.168.12.251"
+  "TargetServerPort".should_be               80
+  "TargetDocumentPath".should_be             "/"
+  "TargetServerSoftware".should_be           "Apache/2.4.4"
+  "FailedRequests".should_be                 0
+  "WriteErrors".should_be                    0
+  "HTMLTransferred".should_be                600
+  "TargetDocumentLength".should_be           6
+  "TotalTransferred".should_be               27500
+  "CompleteRequests".should_be               100
+  "TransferRate".should_be_over              460
+  "TimeTakenforTests".should_be_over         0.01
+  "RequestPerSecond".should_be_over          1000
+  "TimePerRequest".should_be_over            0.5
+  "TimePerConcurrentRequest".should_be_over  5
+end
+
+run
