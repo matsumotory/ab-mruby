02852c0ad2ffc642eb31799c2d29c0118745f492
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-09-07 18:14:40 +0900

    Refactor ab-mruby ext module

--- mrbgems/mruby-ab-mruby-ext/mrblib/test-ext.rb
+++ mrbgems/mruby-ab-mruby-ext/mrblib/test-ext.rb
@@ -2,28 +2,29 @@ module Kernel
   def test_suite &blk
     @@r = get_config
     @@t = blk
+    @@result = true
+  end
+  def bln_color val
+    (val) ? "[\e[33m#{val}\e[m]" : "[\e[36m#{val}\e[m]"
   end
   def should_be val
-    res = @@r[self] == val
-    puts "[TEST CASE] #{self} (#{@@r[self]}) should be #{val}: #{res}"
-    test_fail res
+    ret = @@r[self] == val
+    puts "[TEST CASE] #{bln_color ret} #{self} (#{@@r[self]}) should be #{val}"
+    @@result = ret if ret == false
   end
   def should_be_over val
-    res = @@r[self] >= val
-    puts "[TEST CASE] #{self} (#{@@r[self]}) should be over #{val}: #{res}"
-    test_fail res
+    ret = @@r[self] >= val
+    puts "[TEST CASE] #{bln_color ret} #{self} (#{@@r[self]}) should be over #{val}"
+    @@result = ret if ret == false
   end
   def should_be_under val
-    res = @@r[self] <= val
-    puts "[TEST CASE] #{self} (#{@@r[self]}) should be under #{val}: #{res}"
-    test_fail res
-  end
-  def test_fail res
-    raise "~~~ test failed ~~~" if res == false
+    ret = @@r[self] <= val
+    puts "[TEST CASE] #{bln_color ret} #{self} (#{@@r[self]}) should be under #{val}"
+    @@result = ret if ret == false
   end
   def test_run
     @@t.call
-    puts "=== all test passed ==="
-    true
+    puts "\ntest suites: #{bln_color @@result}\n"
   end
 end
+
--- test/ab-mruby.test.rb
+++ test/ab-mruby.test.rb
@@ -33,36 +33,6 @@
 # "TransferRate"
 #
 
-module Kernel
-  def test_suite &blk
-    @@r = get_config
-    @@t = blk
-    @@result = true
-  end
-  def bln_color val
-    (val) ? "[\e[33m#{val}\e[m]" : "[\e[36m#{val}\e[m]"
-  end
-  def should_be val
-    ret = @@r[self] == val
-    puts "[TEST CASE] #{bln_color ret} #{self} (#{@@r[self]}) should be #{val}"
-    @@result = ret if ret == false
-  end
-  def should_be_over val
-    ret = @@r[self] > val
-    puts "[TEST CASE] #{bln_color ret} #{self} (#{@@r[self]}) should be over #{val}"
-    @@result = ret if ret == false
-  end
-  def should_be_under val
-    ret = @@r[self] < val
-    puts "[TEST CASE] #{bln_color ret} #{self} (#{@@r[self]}) should be under #{val}"
-    @@result = ret if ret == false
-  end
-  def test_run
-    @@t.call
-    puts "\ntest suites: #{bln_color @@result}\n"
-  end
-end
-
 print <<EOS
 ======================================================================
 This is ab-mruby using ApacheBench Version 2.3 <$Revision: 1430300 $>
